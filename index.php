0d40455970a36b2a2087cffc413447deeeefb5f5
Aimal-Maarij <51042919+Aimal-Maarij@users.noreply.github.com>
2021-01-06 12:49:36 +0100

    setup files for the currency exchange project PHP

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Travelling plan</title>
+</head>
+<body>
+
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Brazilian Amount</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Euro</label>
+    <input type="text" class="form-control" id="exampleInputPassword1">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Convert</button>
+</form>
+<div class="result">
+    <h3> Euros </h3>
+</div>
+    
+</body>
+</html>
